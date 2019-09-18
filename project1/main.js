345839e86a84f89c5607232f909782033058cc54
lokacinna <46543827+lokacinna@users.noreply.github.com>
2019-09-17 23:30:22 -0400

    Add files via upload

---
+++ project1/main.js
@@ -0,0 +1,37 @@
+// sets variable to one
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+// next / previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+// determines where the user is in the slideshow
+
+  if (n > slides.length){
+    slideIndex=1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i=0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  // displays slide
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}
